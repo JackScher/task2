ef83594c6ead325280fdbae12a950ecd594aa763
JackScher <dzhek.layt298@gmail.com>
2020-11-03 16:54:43 +0200

    add cors headers

--- task2/task2/settings.py
+++ task2/task2/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
+
     'mptt',
     'rest_framework',
     'rest_framework.authtoken',
@@ -51,12 +53,20 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    # "http://127.0.0.1:9000"
+]
+
 
 ROOT_URLCONF = 'task2.urls'
 
